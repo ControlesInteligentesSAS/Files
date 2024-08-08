0d0bb49323a1500dbf63498c2acc9dea55809e99
Manuel Castro <mcastro@net-support.net>
2024-08-08 15:45:20 -0500

    sh para dowload de los tar
    Shell para descargar los archivos tar.gz

---
+++ dowload_video.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Descarga de ffmpeg.tar,gz
+wget -O ffmpeg.taz.gz https://media.githubusercontent.com/media/ControlesInteligentesSAS/Files/main/ffmpeg.tar.gz?download=true
+
+# Descarga de x264.tar.gz
+wget -O x264.tar.gz https://media.githubusercontent.com/media/ControlesInteligentesSAS/Files/main/x264.tar.gz?download=true
+
